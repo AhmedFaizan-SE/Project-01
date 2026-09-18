76d1b8d300380ca8d60d7d1b1a78575587db4905
Ahmed Faizan <ahmedfaizan.pro@gmail.com>
2026-09-18 23:04:27 +0500

    Set up Passport local authentication

--- app.js
+++ app.js
@@ -11,6 +11,9 @@ const listings = require("./routes/listing.js");
 const reviews = require("./routes/review.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const localStrategy = require("passport-local");
+const User = require("./models/user.js")
 
 // Serve EJS templates from the views folder and static assets from public/.
 app.set("views",path.join(__dirname,"views"));
@@ -53,6 +56,12 @@ app.get("/",(req,res)=>{
 
 app.use(session(sessionOptions));
 app.use(flash());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.use((req, res, next)=>{
     res.locals.success = req.flash("success");
