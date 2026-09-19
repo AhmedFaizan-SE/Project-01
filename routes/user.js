1fe06ff44fc456e2a4497674848fd0dd65630cb4
Ahmed Faizan <ahmedfaizan.pro@gmail.com>
2026-09-20 02:41:26 +0500

    Redirect users after login

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user.js");
 const passport = require("passport");
+const {saveRedirectUrl} = require("../middleware.js")
 
 
 router.get("/signup", (req, res)=> {
@@ -30,9 +31,13 @@ router.get("/login", (req, res)=>{
     res.render("user/login.ejs")
 })
 
-router.post("/login", passport.authenticate("local", { failureRedirect: "/login", failureFlash: true}), async(req, res)=>{
+router.post("/login",saveRedirectUrl,
+     passport.authenticate("local", { failureRedirect: "/login", failureFlash: true}),
+      async(req, res)=>{
    req.flash("success", "Welcome to Airbnb!");
-    res.redirect("/listings");
+        const redirectUrl = res.locals.redirectUrl || "/listings";
+        delete req.session.redirectUrl;
+        res.redirect(redirectUrl);
 })
 
 router.get("/logout", (req, res, next)=>{
